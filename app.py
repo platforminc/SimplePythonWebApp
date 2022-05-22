7883385abc9d9ed08fbc8764cd11d3ffa3b50497
rahmanct <rahman.agoro@carbontrust.com>
2022-05-22 18:40:26 +0100

    commit changes

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is a sample Python Web App running on Flask Framework! This is V3.0 release to SIT.."
+    return "Hello, this is a sample Python Web App running on Flask Framework! This is V3.0 release to UAT"
 
